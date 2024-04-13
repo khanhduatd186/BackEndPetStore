88433e42bb51a76db6cb28cc2dcc7b61c3e226f1
hokhanhdu <khanhduatd1862002@gmail.com>
2024-04-13 23:15:08 +0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,72 @@
+﻿
+using ApiPetShop.Models;
+using ApiPetShop.Repositories;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace ApiPetShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Product_Bill> product_Bills { get; set; }
+        public DbSet<Product_Cart> product_Carts { get; set; }
+        public DbSet<Menu> menus { get; set; }
+        public DbSet<ADV> ADVs { get; set; }
+        public DbSet<Contact> contacts { get; set; }
+   
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // quan he nhiu nhiu bill va product bill
+            builder.Entity<Bill>(b =>
+            {
+                b.ToTable("Bill");
+                b.HasKey(dh => dh.Id);
+                b.Property(dh => dh.dateTime).HasDefaultValueSql("getutcdate()");
+            });
+            builder.Entity<Product_Bill>(pb =>
+            {
+                pb.ToTable("Product_Bill");
+                pb.HasKey(e => new { e.IdProduct, e.IdBill });
+                pb.HasOne(e => e.bill)
+                    .WithMany(e => e.product_Bills)
+                    .HasForeignKey(e => e.IdBill)
+                    .HasConstraintName("FK_ProductBill_Bill");
+                pb.HasOne(e => e.product)
+                    .WithMany(e => e.product_Bills)
+                    .HasForeignKey(e => e.IdProduct)
+                    .HasConstraintName("FK_ProductBill_Product");
+            });
+       
+            builder.Entity<ApplicationUser>(b =>
+            {
+                b.ToTable("ApplicationUser");
+                b.HasKey(dh => dh.Id);
+
+            });
+            builder.Entity<Product_Cart>(pb =>
+            {
+                pb.ToTable("Product_Cart");
+                pb.HasKey(e => new { e.IdUser, e.IdProduct });
+                pb.HasOne(e => e.applicationUser)
+                    .WithMany(e => e.Product_Carts)
+                    .HasForeignKey(e => e.IdUser)
+                    .HasConstraintName("FK_ProductCart_Cart");
+                pb.HasOne(e => e.product)
+                    .WithMany(e => e.product_Carts)
+                    .HasForeignKey(e => e.IdProduct)
+                    .HasConstraintName("FK_ProductCart_Product");
+            });
+           
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiPetShop.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        [Required]
+
+        public string Adddress { get; set; }
+        public virtual ICollection<Bill> Bills { get; set; }
+        public virtual ICollection<Product_Cart> Product_Carts { get; set; }
+        public virtual ICollection<Contact> Contacts { get; set; }
+
+
+    }
+}
